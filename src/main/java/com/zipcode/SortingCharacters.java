9a7d7a8276620e4c05fd2d79b16daaedd84dd65f
Isaac Mills-Owoo <poppymills2@gmail.com>
2025-04-16 13:07:21 -0400

    setting up test and method for sorting string

--- src/main/java/com/zipcode/SortingCharacters.java
+++ src/main/java/com/zipcode/SortingCharacters.java
@@ -1,4 +1,22 @@
 package com.zipcode;
 
 public class SortingCharacters {
+    private String stringToSort;
+
+    public SortingCharacters(String stringToSort) {
+        this.stringToSort = stringToSort;
+    }
+
+    public SortingCharacters() {
+    }
+
+    public String verifyString(String string) {
+        String newStringToSort = string.replaceAll("[^a-zA-Z]", "").toLowerCase();
+        return newStringToSort;
+    }
+
+
+    public String sortString() {
+        return "";
+    }
 }
--- src/test/java/com/zipcode/SortingCharactersTest.java
+++ src/test/java/com/zipcode/SortingCharactersTest.java
@@ -1,14 +1,53 @@
 package com.zipcode;
 
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class SortingCharactersTest {
 
     @Test
-    public void verifyStringTest(){
+    public void verifyStringTest_removeSpaceAndSpecialCharacters(){
         //making sure string returns without special characters
+        SortingCharacters sc = new SortingCharacters();
+        String actualString = sc.verifyString("Sting% cart");
 
+        String expectedString = "stingcart";
+
+        Assert.assertEquals(expectedString, actualString);
+    }
+
+    @Test
+    public void verifyStringTest2_removeSpaceAndSpecialCharacters(){
+        //making sure string returns without special characters
+        SortingCharacters sc = new SortingCharacters();
+        String actualString = sc.verifyString("Z7IpC$O!D#2E@&");
+
+        String expectedString = "zipcode";
+
+        Assert.assertEquals(expectedString, actualString);
+    }
+
+    @Test
+    public void verifyStringTest_isLowerCase_removeSpaceAndSpecialCharacters(){
+        //making sure string returns without special characters and lower case
+        SortingCharacters sc = new SortingCharacters();
+        String actualString = sc.verifyString("CAPITAL ONE &&&");
+
+        String expectedString = "capitalone";
+
+        Assert.assertEquals(expectedString, actualString);
+    }
+
+    @Test
+    public void sortStringTest(){
+
+        SortingCharacters sc = new SortingCharacters("CAPITAL ONE &&&");
+        String actualString = sc.sortString();
+
+        String expectedString = "aaceilnopt";
+
+        Assert.assertEquals(expectedString, actualString);
     }
 
 }
